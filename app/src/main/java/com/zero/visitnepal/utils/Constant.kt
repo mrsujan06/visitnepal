6bc7e1e037ac605602ad3b7d3b801e2ae70a374e
uchit <36070958+uchitsingh@users.noreply.github.com>
2020-01-13 18:22:08 +0000

    Remove apikey from BuildConfigField

--- app/src/main/java/com/zero/visitnepal/utils/Constant.kt
+++ app/src/main/java/com/zero/visitnepal/utils/Constant.kt
@@ -8,7 +8,7 @@ class Constant {
         const val PLACE_URL = "maps/api/place/textsearch/json?"
 
         const val CITY_QUERY = "Top+Cities+In+Nepal"
-        const val API_KEY = BuildConfig.ApiKey
+        const val API_KEY = "AIzaSyBT2bl_XWXG7-fsWtCNyGrTD8wFxaBxbTc"
 
         const val IMAGE_URL = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=600&photoreference="
         const val IMAGE_KEY = "&key="
